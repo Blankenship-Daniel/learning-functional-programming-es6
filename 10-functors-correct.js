d52c03a3c0c958c00da8b240e027605347ed378f
Daniel Blankenship <dblankenship@pro-dblanken1-m.corp.endurance.com>
2017-08-07 14:50:25 -0600

    format comment in lesson 10

--- 10-functors-correct.js
+++ 10-functors-correct.js
@@ -1,13 +1,12 @@
-/*  
-    A functor is an object that has a map function. Arrays in JS 
-    implement map and are therefore functors. Promises, streams, and
-    trees often implement the map function and when they do they are 
-    also functors. The map method of the functor takes the functor 
-    contents and transforms them using the transformation callback
-    that is passed to map. The map then returns a new functor of the
-    same type which contains the transformed values.  
-*/
-
+/**
+ * A functor is an object that has a map function. Arrays in JS 
+ * implement map and are therefore functors. Promises, streams, and
+ * trees often implement the map function and when they do they are 
+ * also functors. The map method of the functor takes the functor 
+ * contents and transforms them using the transformation callback
+ * that is passed to map. The map then returns a new functor of the
+ * same type which contains the transformed values.
+ */
 
 const dragons = [
   { name: 'Fluffykins', health: 70 },
